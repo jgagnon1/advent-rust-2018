0a15cdcff81521001db87faa23a71f5a14611a7c
Jerome Gagnon <jerome.gagnon.1@gmail.com>
2018-12-07 09:24:04 -0500

    Day 5 impl + Move to Rust 2018

---
+++ src/day5/README.md
@@ -0,0 +1,5 @@
+# Day 5: Alchemical Reduction
+
+[Problem description](https://adventofcode.com/2018/day/5) | [Rust solution](./mod.rs)
+
+
--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 fn main() -> Result<(), io::Error> {
 	let matches = App::new("Advent of Rust 2018")
@@ -32,6 +33,7 @@ fn main() -> Result<(), io::Error> {
 		2 => day2::solve(),
 		3 => day3::solve(),
 		4 => day4::solve(),
+		5 => day5::solve(),
 		_ => Ok(())
 	}
 }
